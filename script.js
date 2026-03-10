e0604092ef098d3dfc9dd898f587ccb4f065443e
1301235340-commits <1301235340@s.tusd1.org>
2026-03-10 21:58:06 +0000

    day2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Script started")
+console.log("Script started");
+
+function flipCard(cardId) {
+    console.log("Click");
+    let card = document.getElementById(cardId);
+    card.style.color = "white"
+    card.style.backgroundColor = "Brown"
+}
